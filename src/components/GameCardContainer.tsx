839abb2cb74f2779f70507674988217acf520bfa
Aicirou <akm808.iitkgp@gmail.com>
2023-08-21 13:22:36 +0530

    Displaying genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -6,7 +6,7 @@ interface GameCardContainerProps {
 
 const GameCardContainer = ({ children }: GameCardContainerProps) => {
   return (
-    <Box width={"200px"} borderRadius={10} overflow={"hidden"}>
+    <Box borderRadius={10} overflow={"hidden"}>
       {children}
     </Box>
   );
